a958c9227bd708398093cba7d6c157bdd7ef0b0d
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2018-03-06 00:06:22 +0200

    update fixtures

--- src/player/data-provider/getOtherPlayersData.js
+++ src/player/data-provider/getOtherPlayersData.js
@@ -2,6 +2,6 @@ import { PlayerData } from '../data/PlayerData';
 
 export default () => [
   // new PlayerData(100, 100, 50),
-  new PlayerData(650, 550, 200),
+  new PlayerData(650, 650, 200),
   // new PlayerData(1050, 400, 100),
 ];
