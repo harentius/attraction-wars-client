fda6b4329df1b36eb2ae69ea6bf750b42ce49d43
Yaroslav Osypenko <yaroslav.osypenko@gmail.com>
2019-07-25 21:35:46 +0300

    display loading

--- src/phaser/scenes/Space.js
+++ src/phaser/scenes/Space.js
@@ -17,6 +17,10 @@ class Space extends Phaser.Scene {
   }
 
   preload() {
+    const text = this.add.text(100, 100, 'Loading...', { fontSize: 72 });
+    const zone = this.add.zone(config.width / 2, config.height / 2, config.width, config.height);
+    Phaser.Display.Align.In.Center(text, zone);
+
     this.load.image('background', 'assets/images/space.png');
 
     for (let i = 0; i < 10; i++) {
