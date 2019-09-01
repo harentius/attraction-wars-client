ef13f377965a9b1253ded487898de55b93763392
Sauti <olha.parshakova@gmail.com>
2019-09-01 22:44:23 +0300

    update styles

---
+++ src/ui/Feedback/Feedback.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Feedback.scss';
+
+const Feedback = () => (
+  <a
+    className="feedback"
+    target="_blank"
+    rel="noopener noreferrer"
+    href="https://forms.gle/uTNhXRTeMVygU6nKA"
+  >
+    Feedback
+  </a>
+);
+
+export default Feedback;
